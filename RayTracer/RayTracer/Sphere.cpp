63b58425c17c709002b07a7aa0dc2e5c24f8e58b
Nicholas Gordon <yankeenjg@wl-10-190-53-145.wireless.duke.edu>
2013-11-07 19:06:42 -0500

    fix

--- RayTracer/RayTracer/Sphere.cpp
+++ RayTracer/RayTracer/Sphere.cpp
@@ -14,24 +14,24 @@ bool Sphere::intersect(const Ray& r, float tmin, float tmax, float time, Interse
     double b = 2*dot(r.direction(), temp);
     double c = dot(temp, temp) - radius*radius;
     //discriminant of quadratic equation -- check if > 0 to determine number roots/interesctions
-    double discriminant = b*b - 4*ac;
+    double discriminant = b*b - 4*a*c;
     
     if (discriminant > 0) {//ray intersects sphere at two places
         discriminant = sqrt(discriminant);
         double t = (-b - discriminant) / (2*a);
         if ( t < tmin) {t = (-b + discriminant) / (2*a);} //check if t is within tmin and tmax
-        if (t < timin || t > tmax) {return false;} //t is not within valid interval
+        if (t < tmin || t > tmax) {return false;} //t is not within valid interval
         
         //have a valid intersection
-        rec.t = t;
-        record.normal = normalize(r.origin() + t*r.direct() - center);
+        record.t = t;
+        record.normal = normalize(r.origin() + t*r.direction() - center);
         record.color = color;
         return true;
     }
     return false;
 }
 
-bool Sphere::shadowIntersection(const Ray& r, float tmin, float tmax, float time) const {
+bool Sphere::shadowIntersect(const Ray& r, float tmin, float tmax, float time) const {
     Vector3 temp = r.origin() - center;
     double a = dot(r.direction(), r.direction());
     double b = 2*dot(r.direction(), temp);
@@ -44,7 +44,7 @@ bool Sphere::shadowIntersection(const Ray& r, float tmin, float tmax, float time
         discriminant = sqrt(discriminant);
         double t = (-b - discriminant) / (2*a);
         if (t < tmin) {t = (-b + discriminant) / (2*a);} //check if t is within tmin and tmax
-        if (t < tmin || t > max) {return false;} //t is not within valid interval
+        if (t < tmin || t > tmax) {return false;} //t is not within valid interval
         
         //have a valid interesction
         return true;
