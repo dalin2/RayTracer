f7694f1d3459df60129d35cca6ef73aad4838c9d
Dustin <dalin@wl-10-190-64-13.wireless.duke.edu>
2013-11-11 15:31:15 -0500

    fix

--- RayTracer/RayTracer/Vector3.cpp
+++ RayTracer/RayTracer/Vector3.cpp
@@ -78,7 +78,7 @@ bool operator!=(const Vector3& v1, const Vector3& v2) {
 }
 
 Vector3 operator+(const Vector3& v1, const Vector3& v2) {
-    return Vector3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
+    return Vector3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.());
 }
 
 Vector3 operator-(const Vector3& v1, const Vector3& v2) {
