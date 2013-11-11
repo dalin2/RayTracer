91d607507da90cf74d873a58acbab62db284fdcb
Nicholas Gordon <yankeenjg@wl-10-190-92-112.wireless.duke.edu>
2013-11-11 18:48:35 -0500

    added meshes infrastructure and triangle mesh concrete class

---
+++ RayTracer/DerivedData/RayTracer/Build/Intermediates/RayTracer.build/Debug/RayTracer.build/Objects-normal/x86_64/TriangleMesh.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/TriangleMesh.cpp \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/TriangleMesh.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Shape.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Ray.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Vector3.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Texture.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Vector2.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Color.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Mesh.h \
+  /Users/yankeenjg/RayTracer/RayTracer/RayTracer/Vertex.h
